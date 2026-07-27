4aab28bc475169919e1855146560874dd33f6a2f
nriki7 <nagyriki2012@gmai.com>
2026-07-27 23:41:02 +0300

    Első Discord bot

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+import discord
+from discord.ext import commands
+
+load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Bejelentkezve: {bot.user}")
+
+    print("Szerverek:")
+    for guild in bot.guilds:
+        print(f"- {guild.name} ({guild.id})")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong! 🏓")
+
+bot.run(os.getenv("TOKEN"))
