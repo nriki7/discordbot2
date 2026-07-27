a35cb7dcba6c8fd00913bad7d3f23b202ba10916
nriki7 <nagyriki2012@gmai.com>
2026-07-28 01:42:49 +0300

    Remove dotenv

--- bot.py
+++ bot.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 import discord
 from discord.ext import commands
 
-load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -22,4 +19,5 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong! 🏓")
 
-bot.run(os.getenv("TOKEN"))
+TOKEN = os.getenv("TOKEN")
+bot.run(TOKEN)
